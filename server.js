a6c4d76055611ac19a58a6a91308ef0e0c4f0188
David Cole <misterdavemeister@gmail.com>
2015-12-02 21:41:20 -0600

    "refactored server.js, organized into separate files for requiring"

--- server.js
+++ server.js
@@ -1,54 +1,11 @@
 'use strict';
-var express = require('express'),
-    stylus = require('stylus'),
-    logger = require('morgan'),
-    bodyParser = require('body-parser'),
-    mongoose = require('mongoose');
-
-var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
+var express = require('express');
 var app = express();
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var config = require('./server/config/config')[env];
+require('./server/config/express')(app, config);
+require('./server/config/mongoose')(config);
+require('./server/config/routes')(app);
+app.listen(config.port);
 
-function compile(str, path) {
-  // compile function for stylus
-  return stylus(str).set('filename', path);
-}
-
-app.set('views', __dirname + '/server/views');
-app.set('view engine', 'jade');
-app.use(logger('dev'));
-app.use(bodyParser());
-app.use(stylus.middleware(
-  {
-    src: __dirname + '/public',
-    compile: compile
-  }
-));
-app.use(express.static(__dirname + '/public'));
-
-if (env === 'development') {
-  //$ NODE_ENV=development nodemon server.js
-  mongoose.connect('mongodb://localhost/multivision');
-} else if (env === 'production') {
-  //$ NODE_ENV=production nodemon server.js
-  mongoose.connect('mongodb://dcole:multivision@ds057944.mongolab.com:57944/multivision');
-}
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error...'));
-db.once('open', function callback() {
-  console.log(new Date() + ': ...multivision db opened...');
-});
-
-app.get('/partials/*', function(req, res) {
-  res.render('../../public/app/' + req.params[0])
-});
-
-app.get('*', function(req, res) {
-  res.render('index');
-});
-
-var port = process.env.PORT || 3030;
-app.listen(port);
-
-console.log(new Date() + ': Listening on port ' + port + '...');
+console.log(new Date() + ': Listening on port ' + config.port + '...');

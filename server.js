621c70db429b4a866219fb3d70dd9cfc81cc366d
David Cole <misterdavemeister@gmail.com>
2015-12-10 16:48:23 -0600

    Refactored server-side authorization code

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 'use strict';
-var express = require('express'),
-    mongoose = require('mongoose'),
-    passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy;
-    
+var express = require('express');
 var app = express();
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var config = require('./server/config/config')[env];
@@ -12,36 +8,9 @@ require('./server/config/express')(app, config);
 
 require('./server/config/mongoose')(config);
 
-var User = mongoose.model('User');
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({username: username}).exec(function(err, user){
-      if (user && user.authenticate(password)) {
-        return done(null, user);
-      } else {
-        return done(null, false);
-      }
-    });
-  }
-));
-
-passport.serializeUser(function(user, done) {
-  if (user) {
-    return done(null, user._id);
-  } 
-});
-
-passport.deserializeUser(function(id, done) {
-  User.findOne({_id:id}).exec(function(err, user) {
-    if (user) {
-      return done(null, user);
-    } else {
-      return done(null, false);
-    }
-  });
-});
+require('./server/config/passport')();
 
 require('./server/config/routes')(app);
-app.listen(config.port);
 
+app.listen(config.port);
 console.log(new Date() + ': Listening on port ' + config.port + '...');

34df3b4fdf2424019402456509622ca18bbfbad0
David Cole <misterdavemeister@gmail.com>
2015-12-16 22:03:25 -0600

    multivision tutorial finished. started admin 'view' users

--- public/app/admin/mvUserListCtrl.js
+++ public/app/admin/mvUserListCtrl.js
@@ -1,3 +1,6 @@
 angular.module('app').controller('mvUserListCtrl', function($scope, mvUser) {
    $scope.users = mvUser.query();
+   $scope.viewUser = function(user) {
+      console.log(user);
+   };
 });
